1f4edec91cd399ed33de7052f54699d25cedf3d4
Matt Johnson <mjohnson@jaguarsoft.us>
2012-09-15 12:29:02 -0700

    Added config

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'mattj',
+		'password' => 'N0v4k$0!u7i0n$',
+		'database' => 'timmath',
+	);
+}
